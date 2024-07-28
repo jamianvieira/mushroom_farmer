5e9104f03d464d48dca12c84e5f5a99d56f95660
jamianvieira <jamianvieira@protonmail.com>
2024-07-28 02:23:02 -0700

    re-organized the main resources folder

--- Player/inventory.gd
+++ Player/inventory.gd
@@ -1,5 +1,6 @@
 extends Node
 
+
 var items : Dictionary = {}
 
 func item_pick_up(item_name: String, sprite: Sprite2D):
@@ -7,7 +8,7 @@ func item_pick_up(item_name: String, sprite: Sprite2D):
 	if items.has(item_name):
 		items[item_name] += 1
 		for child in get_parent().get_node("HBoxContainer").get_children():
-			if child.get_node("ItemName").item_name == item_name:
+			if child.item_name == item_name:
 				child.get_node("Count").text = str(items[item_name])
 	else:
 		items[item_name] = 1
@@ -16,6 +17,6 @@ func item_pick_up(item_name: String, sprite: Sprite2D):
 				var inventory_sprite = sprite.duplicate()
 				child.get_node("SpritePlaceholder").add_child(inventory_sprite)
 				child.get_node("Count").text = str(items[item_name])
-				child.get_node("ItemName").item_name = item_name
+				child.item_name = item_name
 				break		
 	print("picked up " + item_name + " in inventory: " + str(items[item_name]))
--- item_name.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-var item_name = ""
